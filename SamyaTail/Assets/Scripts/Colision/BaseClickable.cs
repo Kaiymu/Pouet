d7a1191bbf6a653e8b6099b093780bdafc0f2d09
Tfontaine <tom_1993@hotmail.fr>
2017-05-08 12:25:49 +0200

    - Adding text tween / mouse click distance

---
+++ SamyaTail/Assets/Scripts/Colision/BaseClickable.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseClickable : MonoBehaviour {
+
+    private const float _distanceClick = 1f;
+
+    protected GameObject _samya;
+
+    private void Start()
+    {
+        _samya = GameObject.FindGameObjectWithTag("Samya");
+    }
+
+    bool _calculate = false;
+    void Update () {
+        Renderer _renderer = GetComponent<Renderer>();
+        float distanceSamyaClick = Mathf.Abs(_samya.transform.position.x - _renderer.bounds.center.x);
+
+        if (distanceSamyaClick > _distanceClick)
+        {
+            _calculate = true;
+            return;
+        }
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            Vector3 pos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(pos, Vector2.zero);
+            if (hit != null && hit.collider != null)
+            {
+                _OnClickedObject();
+            }
+        }
+
+        if(_calculate)
+        {
+            float distanceSamyaClick2 = Mathf.Abs(_samya.transform.position.x - _renderer.bounds.center.x);
+
+            Debug.LogError(distanceSamyaClick2 + " " + _distanceClick);
+            if (distanceSamyaClick2 < _distanceClick)
+            {
+                _calculate = false;
+                _OnClickedObject();
+            }
+        }
+    }
+
+    protected abstract void _OnClickedObject();
+}
---
+++ SamyaTail/Assets/Scripts/Colision/TextClickable.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextClickable : BaseClickable {
+
+    public TextMesh[] _3DText;
+
+
+    protected override void _OnClickedObject()
+    {
+        for(int i = 0; i < _3DText.Length; i++)
+        {
+            _3DText[i].gameObject.SetActive(true);
+        }
+
+    }
+}
